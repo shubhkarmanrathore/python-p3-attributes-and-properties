d1e603eaa33a769b2bdfda4200d88b1985138cb7
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-07-19 13:57:45 -0700

    adds lab

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,30 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name="Guido", job="Admin"):
+        self.name=name
+        self.job=job
+
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if ((type(name) == str) and ( 1 <= len(name) <= 25)) :
+            self._name = name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    name=property(get_name,set_name)               
+
+    def get_job(self):
+        return self._job 
+    
+    def set_job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = job
+
+    job = property(get_job, set_job)  
+
+
+Person(job="Software Engineer")
